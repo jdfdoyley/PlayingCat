911de09dd7c7a0b2c48de11ff6c1120b0a43d566
Jason D'Oyley <jdfdoyley@gmail.com>
2023-01-14 23:14:06 -0500

    Code to check if cat is playing

--- src/App.java
+++ src/App.java
@@ -6,4 +6,20 @@
  */
 
 public class App {
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 10));     // false
+        System.out.println(isCatPlaying(true, 36));     // true
+        System.out.println(isCatPlaying(false, 36));    // false
+        System.out.println(isCatPlaying(false, 35));    // true
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        boolean output = summer && (temperature >= 25 && temperature <= 45);
+
+        if (!summer && (temperature >= 25 && temperature <= 35)) {
+            output = true;
+        }
+
+        return output;
+    }
 }
